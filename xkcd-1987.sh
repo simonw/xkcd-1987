3f76b10d91e3bf9d5c1cd1f5b537b9f22a836007
Simon Willison <swillison@gmail.com>
2020-08-18 22:39:35 -0700

    Fixed Python 2 --version, thanks @imomaliev - closes #1

--- xkcd-1987.sh
+++ xkcd-1987.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "which python:" $(which python)
 echo "which python3:" $(which python3)
-echo "python --version:" $( { python --version } 2>&1 )
 echo "python3 --version:" $(python3 --version)
+echo "python --version:" $( python --version 2>&1 )
